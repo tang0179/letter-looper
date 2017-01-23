77c8fd3cd64f8f6ca776569b4973bb559b6deefc
tang0179 <tang0179@algonquinlive.com>
2017-01-23 16:46:46 -0500

    Update looper.js

--- js/looper.js
+++ js/looper.js
@@ -1,1 +1,16 @@
+var letter;
+var k;
 
+while (!letter) {
+   letter = prompt("Enter a letter please");
+}
+
+console.log();
+
+
+for(var i=0;i<10;i++){
+    for(var k=0;k<=i;k++){
+        document.write(letter);
+    }
+ document.write("<br>");
+}
